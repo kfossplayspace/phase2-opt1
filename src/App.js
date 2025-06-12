8292c253519f1b4e7438bd1b5ff0387ae38df943
kim foss <fosskm@msn.com>
2025-06-12 14:44:40 -0400

    secondcommit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { getAll, post, put, deleteById } from './memdb.js';
 import './App.css';
 import { CustomerList } from './components/CustomerList.js';
-
+import { CustomerAddUpdateForm } from './components/CustomerAddUpdateForm.js';
 
 
 function log(message){console.log(message);}
@@ -66,12 +66,6 @@ export function App(params) {
 
   }
 
- // function CustomerList(params){
- //    return (
-//     <div className="boxed">
-  //    <h4>Customer List</h4>
- //    </div> );
- // }
   return (
     <div>
        <CustomerList 
@@ -80,55 +74,19 @@ export function App(params) {
         handleListClick={handleListClick} 
       />
 
-       
-    <div className="boxed">
-      <div>
-        <h4>{mode}</h4>
+  
+       <CustomerAddUpdateForm
+        formObject={formObject}
+        handleInputChange={handleInputChange}
+        onDeleteClick={onDeleteClick}
+        onSaveClick={onSaveClick}
+        onCancelClick={onCancelClick}
+        mode={mode}
+      />
       </div>
-      <form >
-        <table id="customer-add-update" >
-          <tbody>
-            <tr>
-              <td className={'label'} >Name:</td>
-              <td><input
-                type="text"
-                name="name"
-                onChange={(e) => handleInputChange(e)}
-                value={formObject.name}
-                placeholder="Customer Name"
-                required /></td>
-            </tr>
-            <tr>
-              <td className={'label'} >Email:</td>
-              <td><input
-                type="email"
-                name="email"
-                onChange={(e) => handleInputChange(e)}
-                value={formObject.email}
-                placeholder="name@company.com" /></td>
-            </tr>
-            <tr>
-              <td className={'label'} >Pass:</td>
-              <td><input
-                type="text"
-                name="password"
-                onChange={(e) => handleInputChange(e)}
-                value={formObject.password}
-                placeholder="password" /></td>
-            </tr>
-            <tr className="button-bar">
-              <td colSpan="2">
-                <input type="button" value="Delete" onClick={onDeleteClick} />
-                <input type="button" value="Save" onClick={onSaveClick} />
-                <input type="button" value="Cancel" onClick={onCancelClick} />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </form>
-    </div>
-    </div>
+  
   );
-}
-
+ 
+}     
+  
 export default App;
